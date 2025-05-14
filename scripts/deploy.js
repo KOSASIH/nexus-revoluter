15df7db45c3f216471d96670e83ee9befa7b9533
KOSASIH <kosasihg88@gmail.com>
2025-05-14 20:29:37 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 const { ethers, upgrades } = require('hardhat');
 
 async function main() {
+  // Deploy Nexus
   const Nexus = await ethers.getContractFactory('Nexus');
   console.log('Deploying Nexus...');
   const nexus = await upgrades.deployProxy(Nexus, ['Nexus Revoluter', 'NEXUS', ethers.parseEther('0.0001')], {
@@ -8,7 +9,18 @@ async function main() {
     kind: 'uups',
   });
   await nexus.waitForDeployment();
-  console.log(`Nexus deployed to: ${await nexus.getAddress()}`);
+  const nexusAddress = await nexus.getAddress();
+  console.log(`Nexus deployed to: ${nexusAddress}`);
+
+  // Deploy Lock
+  const Lock = await ethers.getContractFactory('Lock');
+  console.log('Deploying Lock...');
+  const lock = await upgrades.deployProxy(Lock, [nexusAddress], {
+    initializer: 'initialize',
+    kind: 'uups',
+  });
+  await lock.waitForDeployment();
+  console.log(`Lock deployed to: ${await lock.getAddress()}`);
 }
 
 main().catch((error) => {
